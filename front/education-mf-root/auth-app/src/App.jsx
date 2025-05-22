72043fd195ae6bc8309c2629346ad9e317580672
Diana <3339787@mail.ru>
2025-05-22 03:34:09 +0300

    микрофронт

---
+++ front/education-mf-root/auth-app/src/App.jsx
@@ -0,0 +1,12 @@
+import AuthorisationPage from "./pages/AuthorisationPage";
+
+function App() {
+  return (
+    <div>
+      <h2>Auth App</h2>
+      <AuthorisationPage />
+    </div>
+  );
+}
+
+export default App;
