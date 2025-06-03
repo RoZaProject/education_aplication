62c26b70e1534614e7d6e63205d801be2d55bf22
ZdianaDi <3339787@mail.ru>
2025-06-03 19:58:27 +0300

    Delete front/back directory

--- front/back/utils/variable_environment.py
+++
@@ -1,8 +0,0 @@
-import os
-class VarEnv:
-    DBUSER = os.environ.get("DBUSER")
-    DBPASSWORD = os.environ.get("DBPASSWORD")
-    DBHOST = os.environ.get("DBHOST")
-    DBNAME = os.environ.get("DBNAME")
-    SECRET_KEY = os.environ.get("SECRET_KEY")
-    GIGAGPT = os.environ.get("GIGAGPT")
